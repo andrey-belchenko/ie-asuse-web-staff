83fea4b220379d7a11586f3f52f680e394312d04
Andrey Belchenko <abelchenko@conteq.ru>
2024-07-18 11:58:37 +0300

    -

--- back/exchange-api/README.md
+++
@@ -1,6 +0,0 @@
-За основу взят
-https://github.com/snava10/example-typescript-rest-api-app
-, но удалил из него почти все содержимое
-
-Для публикации в docker, взял пример отсюда
-https://cloudnweb.dev/2019/09/building-a-production-ready-node-js-app-with-typescript-and-docker/
--- back/exchange-api/package-df.bat
+++
@@ -1,5 +0,0 @@
-docker compose --project-directory .  --file ./docker-compose.yml  up   --build -d
-docker tag exchange-api dockerhub.oastu.lan:5000/exchange-api:v230416_1
-docker push dockerhub.oastu.lan:5000/exchange-api:v230416_1
-docker compose --project-directory .  --file ./docker-compose.yml   down
-
