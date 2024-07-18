c8a2d812faced0236326d7b65aae196e3fd87ba8
Andrey Belchenko <abelchenko@conteq.ru>
2024-07-18 18:17:16 +0300

    1

--- front/vue-3-typescript-example/src/main.ts
+++ front/vue-3-typescript-example/src/main.ts
@@ -3,5 +3,6 @@ import App from "@/App.vue";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import router from "@/router";
+import "devextreme/dist/css/dx.light.css";
 
 createApp(App).use(router).mount("#app");
