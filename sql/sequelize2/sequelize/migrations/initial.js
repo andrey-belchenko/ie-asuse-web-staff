d8738ac06294d2983cf40d34428ef79b9e0c3118
Andrey Belchenko <abelchenko@conteq.ru>
2024-07-27 17:42:05 +0300

    sequlize 2

--- sql/sequelize2/sequelize/migrations/initial.js
+++ sql/sequelize2/sequelize/migrations/initial.js
@@ -3,8 +3,9 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.query(`
+      DROP VIEW IF EXISTS public.sales_view CASCADE;
       CREATE OR REPLACE VIEW public.sales_view
-      AS select customer from  public.sales s 
+      AS select product from  public.sales s;
     `);
   },
 };
