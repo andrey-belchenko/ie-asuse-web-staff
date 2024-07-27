9729422164a76bfd6b7dedd1ab519ebf1e634707
Andrey Belchenko <abelchenko@conteq.ru>
2024-07-27 17:04:46 +0300

    sequlize 2

---
+++ sql/sequelize2/sequelize/scripts/clearDbMigrationHistory.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require(path.join(__dirname, '../config/config.json'))[env];
+
+const sequelize = new Sequelize(config);
+
+sequelize.query('DROP TABLE public."SequelizeMeta"')
+  .then(function() {
+    console.log('Query executed successfully');
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
