b30486bdcade81d57d2a7967103851948dc547fc
Andrey Belchenko <abelchenko@conteq.ru>
2024-08-06 01:55:09 +0300

    in progress сальдо

--- sql/sequelize2/src/declaration/ddl/report_dm/tables/msr_фин_обор.sql
+++ sql/sequelize2/src/declaration/ddl/report_dm/tables/msr_фин_обор.sql
@@ -2,24 +2,8 @@ CREATE TABLE report_dm.msr_фин_обор (
 	договор_ид int4 NULL,
 	вид_реал_ид int4 NULL,
 	дата timestamptz NULL,
-	период_ид numeric(6, 2) NULL,
-	док_нач_ид int4 NULL,
-	вид_тов_ид int4 NULL,
-	опл_ид int4 NULL,
-	тип_опл_ид int4 NULL,
-	исх_опл_ид int4 NULL,
-	начисл numeric NULL,
-	опл numeric NULL,
-	погаш numeric NULL,
-	начисл_осн numeric NULL,
-	погаш_осн numeric NULL,
-	опл_кред numeric NULL,
-	сторно_кред numeric NULL,
-	обор numeric NULL,
 	обор_деб numeric NULL,
-	обор_осн_деб numeric NULL,
-	обор_осн_кред numeric NULL,
-	обор_осн numeric NULL
+	обор_кред numeric NULL
 );
 CREATE INDEX i_msr_фин_обор_договор_ид ON report_dm.msr_фин_обор USING btree(договор_ид);
 CREATE INDEX i_msr_фин_обор_дата ON report_dm.msr_фин_обор USING btree(дата);
--- sql/sequelize2/src/scripts/reset.ts
+++ sql/sequelize2/src/scripts/reset.ts
@@ -1,14 +1,18 @@
 import declaration from "../declaration/obj";
 import fill_dim_data from "../declaration/obj/report_dm/functions/fill_dim_дата";
 import fill_msr_фин_начисл from "../declaration/obj/report_dm/functions/fill_msr_фин_начисл";
+import fill_msr_фин_обор from "../declaration/obj/report_dm/functions/fill_msr_фин_обор";
 import fill_msr_фин_опл from "../declaration/obj/report_dm/functions/fill_msr_фин_опл";
 import fill_msr_фин_опл_кредит from "../declaration/obj/report_dm/functions/fill_msr_фин_опл_кредит";
 import fill_msr_фин_опл_погаш from "../declaration/obj/report_dm/functions/fill_msr_фин_опл_погаш";
+import fill_msr_фин_сальдо_по_дог_вид_реал from "../declaration/obj/report_dm/functions/fill_msr_фин_сальдо_по_дог_вид_реал";
 import dim_дата from "../declaration/obj/report_dm/tables/dim_дата";
 import msr_фин_начисл from "../declaration/obj/report_dm/tables/msr_фин_начисл";
+import msr_фин_обор from "../declaration/obj/report_dm/tables/msr_фин_обор";
 import msr_фин_опл from "../declaration/obj/report_dm/tables/msr_фин_опл";
 import msr_фин_опл_кредит from "../declaration/obj/report_dm/tables/msr_фин_опл_кредит";
 import msr_фин_опл_погаш from "../declaration/obj/report_dm/tables/msr_фин_опл_погаш";
+import msr_фин_сальдо_по_дог_вид_реал from "../declaration/obj/report_dm/tables/msr_фин_сальдо_по_дог_вид_реал";
 import msr_фин from "../declaration/obj/report_dm/views/msr_фин";
 import fill_фин_опл from "../declaration/obj/report_stg/functions/fill_фин_опл";
 import фин_опл from "../declaration/obj/report_stg/tables/фин_опл";
@@ -24,8 +28,12 @@ const run = async () => {
     // msr_фин_опл_кредит,
     // fill_msr_фин_опл_кредит
     // msr_фин,
-    msr_фин_начисл,
-    fill_msr_фин_начисл
+    // msr_фин_начисл,
+    // fill_msr_фин_начисл
+    // msr_фин_обор,
+    // fill_msr_фин_обор,
+    msr_фин_сальдо_по_дог_вид_реал,
+    fill_msr_фин_сальдо_по_дог_вид_реал
   ]);
   console.log("done");
 };
