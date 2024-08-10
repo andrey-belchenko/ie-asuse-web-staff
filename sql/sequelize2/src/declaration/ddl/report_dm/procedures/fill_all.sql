9c850fe8e8eb46167a65c30240c7f7c41b46c7cd
Andrey Belchenko <abelchenko@conteq.ru>
2024-08-10 22:11:31 +0300

    rearrange

--- sql/sequelize2/src/declaration/ddl/report_dm/procedures/fill_all.sql
+++ sql/sequelize2/src/declaration/ddl/report_dm/procedures/fill_all.sql
@@ -1,10 +1,9 @@
 CREATE OR REPLACE PROCEDURE report_dm.fill_all () LANGUAGE plpgsql AS $$ BEGIN 
 call report_dm.fill_dim_дата();
 call report_dm.fill_msr_фин_начисл();
-call report_dm.fill_msr_фин_обор();
 call report_dm.fill_msr_фин_опл_кредит();
 call report_dm.fill_msr_фин_опл_погаш();
-call report_dm.fill_msr_фин_опл();
+call report_dm.fill_msr_фин_обор();
 call report_dm.fill_msr_фин_сальдо_по_дог_вид_реал();
 -- call report_dm.fill_msr_фин_сальдо_по_дог();
 -- call report_dm.fill_msr_фин();
--- sql/sequelize2/src/declaration/obj/report_dm/procedures/index.ts
+++ sql/sequelize2/src/declaration/obj/report_dm/procedures/index.ts
@@ -1,20 +1,21 @@
 import fill_all from "./fill_all";
-import fill_dim_data from "./fill_dim_дата";
+import fill_dim_дата from "./fill_dim_дата";
 import fill_msr_фин from "./fill_msr_фин";
 import fill_msr_фин_начисл from "./fill_msr_фин_начисл";
 import fill_msr_фин_обор from "./fill_msr_фин_обор";
 import fill_msr_фин_опл_кредит from "./fill_msr_фин_опл_кредит";
 import fill_msr_фин_опл_погаш from "./fill_msr_фин_опл_погаш";
-import fill_msr_фин_сальдо from "./fill_msr_фин_сальдо_по_дог";
-
+import fill_msr_фин_сальдо_по_дог from "./fill_msr_фин_сальдо_по_дог";
+import fill_msr_фин_сальдо_по_дог_вид_реал from "./fill_msr_фин_сальдо_по_дог_вид_реал";
 
 export default [
-  fill_all,
+  fill_dim_дата,
   fill_msr_фин_начисл,
-  fill_msr_фин_обор,
-  fill_msr_фин_сальдо,
-  fill_dim_data,
   fill_msr_фин_опл_погаш,
   fill_msr_фин_опл_кредит,
+  fill_msr_фин_обор,
+  fill_msr_фин_сальдо_по_дог_вид_реал,
+  fill_msr_фин_сальдо_по_дог,
   fill_msr_фин,
+  fill_all,
 ];
