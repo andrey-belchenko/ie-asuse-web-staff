37676866deec3a26f2347493a7120b969144ade1
Andrey Belchenko <abelchenko@conteq.ru>
2024-08-11 15:08:40 +0300

    incremental refresh dm checking

--- sql/sequelize2/sandbox/refresh_slice_init.sql
+++ sql/sequelize2/sandbox/refresh_slice_init.sql
@@ -5,10 +5,13 @@ select * from report_stg.refresh_slice
 
 insert into report_stg.refresh_slice(договор_id, дата_c, дата_по) values (358,'1900-01-01','3000-01-01')
 
+insert into report_stg.refresh_slice(договор_id, дата_c, дата_по) values (400002594,'1900-01-01','3000-01-01')
+
+
 insert into report_stg.refresh_slice(договор_id, дата_c, дата_по) values (358,'2022-03-01'::date, '2022-03-31'::date)
 
 insert into report_stg.refresh_slice (договор_id, дата_c, дата_по)
-select kod_dog , '2022-03-01'::date, '2022-03-31'::date from kr_dogovor;
+select kod_dog , '2022-10-01'::date, '2022-10-31'::date from kr_dogovor where dep=1212;
 
 
 
