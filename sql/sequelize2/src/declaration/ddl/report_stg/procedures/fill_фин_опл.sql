99381de6524de606e47aee5e2602594ee7ef003a
Andrey Belchenko <abelchenko@conteq.ru>
2024-08-17 20:23:03 +0300

    archive

--- sql/sequelize2/src/declaration/ddl/report_stg/procedures/fill_фин_опл.sql
+++ sql/sequelize2/src/declaration/ddl/report_stg/procedures/fill_фин_опл.sql
@@ -2,6 +2,30 @@ CREATE OR REPLACE PROCEDURE report_stg.fill_фин_опл () LANGUAGE plpgsql AS
 DELETE FROM report_stg.фин_опл a USING report_stg.refresh_slice rs
 WHERE rs.договор_id = a.договор_id
     AND a.дата BETWEEN rs.дата_c AND rs.дата_по;
+CREATE TEMP TABLE tmp1 ON COMMIT DROP AS
+select rs.refresh_slice_id,
+    o.kod_dog as договор_id,
+    o.kod_opl as опл_id,
+    o.vid_real AS вид_реал_id,
+    o.kod_type_opl as тип_опл_id,
+    o.kod_parent as исх_опл_id,
+    o.kod_sf as док_нач_id,
+    o.dat_uch as дата,
+    o.ym AS период_id,
+    coalesce(o.opl, 0) + coalesce(o.opls, 0) as опл,
+    COALESCE(
+        clo.дата_закрытия,
+        clop.дата_закрытия,
+        cln.дата_закрытия,
+        '2100-01-01'::date
+    ) дата_закрытия,
+    rs.дата_архивации
+from sr_opl o
+    join report_stg.refresh_slice rs on rs.договор_id = o.kod_dog
+    and o.ym BETWEEN rs.период_с and rs.период_по
+    LEFT JOIN report_stg.фин_закрытие_начисл cln on cln.док_нач_id = o.kod_sf
+    LEFT JOIN report_stg.фин_закрытие_кред clo on clo.опл_id = o.kod_opl
+    LEFT JOIN report_stg.фин_закрытие_кред clop on clop.опл_id = o.kod_parent;
 INSERT INTO report_stg.фин_опл (
         refresh_slice_id,
         договор_id,
@@ -13,38 +37,14 @@ INSERT INTO report_stg.фин_опл (
         дата,
         период_id,
         опл
-    ) with x0 as (
-        select rs.refresh_slice_id,
-            o.kod_dog as договор_id,
-            o.kod_opl as опл_id,
-            o.vid_real AS вид_реал_id,
-            o.kod_type_opl as тип_опл_id,
-            o.kod_parent as исх_опл_id,
-            o.kod_sf as док_нач_id,
-            o.dat_uch as дата,
-            o.ym AS период_id,
-            coalesce(o.opl, 0) + coalesce(o.opls, 0) as опл,
-            COALESCE(
-                clo.дата_закрытия,
-                clop.дата_закрытия,
-                cln.дата_закрытия,
-                '2100-01-01'::date
-            ) дата_закрытия,
-            rs.дата_архивации
-        from sr_opl o
-            join report_stg.refresh_slice rs on rs.договор_id = o.kod_dog
-            and o.ym BETWEEN rs.период_с and rs.период_по
-            LEFT JOIN report_stg.фин_закрытие_начисл cln on cln.док_нач_id = o.kod_sf
-            LEFT JOIN report_stg.фин_закрытие_кред clo on clo.опл_id = o.kod_opl
-            LEFT JOIN report_stg.фин_закрытие_кред clop on clop.опл_id = o.kod_parent
-    ),
+    ) with 
     x as (
         select a.*,
             case
                 when a.дата_закрытия <= a.дата_архивации then 1
                 else 0
             end as архив
-        from x0 a
+        from tmp1 a
     ),
     x1 as (
         select refresh_slice_id,
