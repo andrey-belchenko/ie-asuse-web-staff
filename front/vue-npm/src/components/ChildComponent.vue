648c43ba55825c63a568fcca60431289ff17eab5
Andrey Belchenko <abelchenko@conteq.ru>
2024-10-22 22:43:15 +0300

    1

---
+++ front/vue-npm/src/components/ChildComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>{{ message }}</h2>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const message = ref('Hello from ChildComponent');
+</script>
---
+++ front/vue-npm/src/components/ParentComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <!-- <h1>{{ message }}</h1> -->
+        <ChildComponent />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ChildComponent from './ChildComponent.vue';
+
+const message = ref('Hello from ParentComponent');
+</script>
