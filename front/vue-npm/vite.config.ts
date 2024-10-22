e4c6786229d9a8aa3d406102fa54c01849c22d96
Andrey Belchenko <abelchenko@conteq.ru>
2024-10-23 00:36:05 +0300

    1

--- front/vue-npm/vite.config.ts
+++ front/vue-npm/vite.config.ts
@@ -1,14 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+      name: 'VueNpm',
+      fileName: format => `vue-npm.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
     },
   },
 })
