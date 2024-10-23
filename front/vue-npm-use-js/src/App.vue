0f250207ff597a283c7ce6a7c69834a9f745033b
Andrey Belchenko <abelchenko@conteq.ru>
2024-10-23 04:16:27 +0300

    1

--- front/vue-npm-use-js/src/App.vue
+++ front/vue-npm-use-js/src/App.vue
@@ -1,7 +1,23 @@
 <script setup>
 import { ParentComponent } from 'il-reports'
+import { Reports } from 'ai-reports-ui'
 </script>
 
-<template>
+<!-- <template>
   <ParentComponent />
+</template> -->
+
+
+<template>
+  <div class="main">
+    <Reports/>
+  </div>
 </template>
+
+
+<style scoped>
+.main {
+  position: absolute;
+  inset: 0;
+}
+</style>
--- front/vue-npm-use-js/src/main.js
+++ front/vue-npm-use-js/src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
-
+import 'devextreme/dist/css/dx.material.blue.light.compact.css';
+import 'ai-reports-ui/dist/style.css';
 import { createApp } from 'vue'
 import App from './App.vue'
 
